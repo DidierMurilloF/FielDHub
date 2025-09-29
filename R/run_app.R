d185cea5059173a0f0000022f5f654f810cc1302
andres <afquinteromoreano@gmail.com>
2025-09-29 18:20:02 -0500

    feat: Allow the user to customize launch.browser

--- R/run_app.R
+++ R/run_app.R
@@ -2,21 +2,22 @@
 #'
 #' @return A shiny app object
 #' @param ... Unused, for extensibility
-#' 
+#'
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 
 run_app <- function(
-  ...
+  ...,
+  launch.browser = TRUE
 ) {
-  options(shiny.maxRequestSize=100*1024^2)
+  options(shiny.maxRequestSize = 100 * 1024^2)
   with_golem_options(
     app = shinyApp(
-      options = list(launch.browser = TRUE),
-      ui = app_ui, 
+      options = list(launch.browser = launch.browser),
+      ui = app_ui,
       server = app_server
-    ), 
+    ),
     golem_opts = list()
   )
 }
